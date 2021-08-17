8bcbd0b8cd4590ec339a8626152144918bcb7ec8
quangtuan28200 <buiquangtuan02082000@gmail.com>
2021-08-17 16:42:56 +0700

    search by speech

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,6 @@ function setData(city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_ID}&units=metric`)
         .then(async res => {
             const data = await res.json();
-            console.log(data)
             cityName.innerHTML = data.name || DEFAULT_VALUE;
             weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
             weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
@@ -36,4 +35,92 @@ function setData(city) {
                 document.querySelector('.loader').classList.add("hidden");
             }, 400);
         })
+}
+
+//search with speech---------------------------------------------------------
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+const synth = window.speechSynthesis;
+recognition.lang = 'vi-VI';
+recognition.continuous = false;// du lieu tra ve ngay
+
+const microphone = document.querySelector('.microphone');
+
+const speak = (text) => {
+    if (synth.speaking) {
+        console.error('Busy. Speaking...');
+        return;
+    }
+
+    const utter = new SpeechSynthesisUtterance(text);
+
+    utter.onend = () => {
+        console.log('SpeechSynthesisUtterance.onend');
+    }
+    utter.onerror = (err) => {
+        console.error('SpeechSynthesisUtterance.onerror', err);
+    }
+
+    synth.speak(utter);
+};
+
+const handleVoice = (text) => {
+    console.log('text', text);
+
+    // "thời tiết tại Đà Nẵng" => ["thời tiết tại", "Đà Nẵng"]
+    const handledText = text.toLowerCase();
+    if (handledText.includes('thời tiết tại')) {
+        const location = handledText.split('tại')[1].trim();
+
+        console.log('location', location);
+        searchInput.value = location;
+        const changeEvent = new Event('change');
+        searchInput.dispatchEvent(changeEvent);
+        return;
+    }
+    
+    const container = document.querySelector('.container');
+    if (handledText.includes('thay đổi màu nền')) {
+        const color = handledText.split('màu nền')[1].trim();
+        container.style.background = color;
+        return;
+    }
+
+    if (handledText.includes('màu nền mặc định')) {
+        container.style.background = '';
+        return;
+    }
+
+    if (handledText.includes('mấy giờ')) {
+        const textToSpeech = `${moment().hours()} hours ${moment().minutes()} minutes`;
+        speak(textToSpeech);
+        return;
+    }
+
+    speak('Try again');
+}
+
+microphone.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    recognition.start();
+    microphone.classList.add('recording');
+});
+
+recognition.onspeechend = () => {
+    recognition.stop();
+    microphone.classList.remove('recording');
+}
+
+recognition.onerror = (err) => {
+    console.error(err);
+    microphone.classList.remove('recording');
+}
+
+recognition.onresult = (e) => {
+    console.log('onresult', e);
+    const text = e.results[0][0].transcript;
+    handleVoice(text);
 }
